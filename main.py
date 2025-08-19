4529fc7dee4707c61cfbbf7b6b17af2ba793cebe
peysonsmith <peysonsmith@gmail.com>
2025-08-19 00:55:25 -0700

    created main and laid out steps

---
+++ main.py
@@ -0,0 +1,14 @@
+# Create deck of cards each with a value and suit
+
+# Create a random that is able to pull a card from the deck and take it out of the remaining cards
+
+# Create a pile that remembers how many cards are on that pile and what the top card of that pile is
+ 
+# Create a new pile of the random card that can be placed 1 pile away or 3 piles away if the suit or number
+# match of the top card of either of those piles and the current card
+ 
+# Can now move any of the remaining piles if they are able to be played
+
+# Restrict the player from drawing a new card if a move can currently be played
+
+# After all the cards have been gone through, tally up the number of points the player recieves
